f5c7e55445edac1d495de4bcf44c95515a4af89a
Cora Janse <Cora.Janse@gmail.com>
2018-07-06 13:23:16 +0200

    Initial set-up as maven project

---
+++ src/test/java/cx/samplecode/graphexperiment/GraphExperimentRunnerTest.java
@@ -0,0 +1,13 @@
+package cx.samplecode.graphexperiment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GraphExperimentRunnerTest {
+
+    @Test
+    void testReadGraphFromJson() {
+        Assertions.fail("This function is not yet implemented");
+    }
+
+}
